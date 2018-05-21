163c5dff40d856efe8459b93a1783ea9c0d623e6
papandadz <marija.nakova996@gmail.com>
2018-05-21 14:52:39 +0200

    Making backend and pulling data from spotify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import  queryString from 'query-string';
 let defaultStyle={
   color:"black"
 };
@@ -109,21 +110,35 @@ class App extends Component {
     }
   }
   componentDidMount(){
-    setTimeout(()=> {
-    this.setState({serverData:fakeServerData});
-  },2000);
+    let parsed = queryString.parse(window.location.search);
+    let accessToken = parsed.access_token;
+
+    fetch('https://api.spotify.com/v1/me', {
+      headers:
+      {'Authorization': 'Bearer' + accessToken}} 
+      ).then(response => response.json())
+      .then(data => this.setState({serverData:{ user : { name : {data.display_name}}}))
+
+    fetch('https://api.spotify.com/v1/me/playlists', {
+      headers:
+      {'Authorization': 'Bearer' + accessToken}} 
+      ).then(response => response.json())
+      .then(data => this.setState({serverData:{ user : { playlists : {data.display_name}}}))
+    
   }
   render() {
-    let playlistToRender = this.state.serverData.user ? this.state.serverData.user.playlists.
-    filter(playlist=> 
-      playlist.name.toLowerCase().includes(
-        this.state.filterString.toLowerCase())
-    ) : []
+    let playlistToRender = 
+    this.state.serverData.user && 
+    this.state.serverData.user.playlists 
+    ?  this.state.serverData.user.playlists.filter(playlist=> 
+       playlist.name.toLowerCase().includes(
+        this.state.filterString.toLowerCase()))
+    : []
     return (
       <div className="App">
         {this.state.serverData.user ?
           <div>
-          <h1 style={{...defaultStyle, 'font-size':'54px'}}>
+          <h1 style={{...defaultStyle, 'fontSize':'54px'}}>
             {this.state.serverData.user.name}'s Playlists
           </h1>
           <PlayListCounter playlists={playlistToRender}/>
@@ -134,8 +149,10 @@ class App extends Component {
           {playlistToRender.map(playlist=> 
               <Playlist playlist={playlist}/>
             )}
-        </div> : <h1 style={defaultStyle}>Loading...</h1>
+        </div> : <button onClick={() => window.location='http://localhost:8888/login'}
+         style={{padding: "20px", "fontSize": "50px", "marginTop":"20px"}}>Sign in with Spotify</button>
         }
+        
       </div>
     );
   }
