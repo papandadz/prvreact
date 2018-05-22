a289def966ece37677e59890c4ecec7bd32ff8cc
papandadz <marija.nakova996@gmail.com>
2018-05-22 11:52:37 +0200

    fixed button to also work in production

--- src/App.js
+++ src/App.js
@@ -162,7 +162,11 @@ class App extends Component {
           {playlistToRender.map(playlist=> 
               <Playlist playlist={playlist}/>
             )}
-        </div> : <button onClick={() => window.location='http://localhost:8888/login'}
+        </div> : <button onClick={() => {
+          window.location=window.location.includes('localhost') 
+            ? 'http://localhost:8888/login' 
+            : 'https://prvreact.herokuapp.com/login'}
+        }
          style={{padding: "20px", "fontSize": "50px", "marginTop":"20px"}}>Sign in with Spotify</button>
         }
         
